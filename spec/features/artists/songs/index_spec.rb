4b385395f19fd50248a3e5edeb24b70c730018fc
Matt Toensing <matthew.toensing@gmail.com>
2021-07-07 14:44:45 -0700

    Add feature tests

--- spec/features/artists/songs/index_spec.rb
+++ spec/features/artists/songs/index_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe 'Artists songs index' do
     expect(page).to have_content(dirt_road.title)
   end
 
-  it 'links to each songs show page'
+  it 'links to each songs show page' do
+    clutch = Artist.create!(name: "Clutch")
+    regulator = clutch.songs.create!(title: "Regulator", length: 234, play_count: 123)
+    dirt_road = clutch.songs.create!(title: "Dirt Road", length: 984, play_count: 586)
+
+    visit "/artists/#{clutch.id}/songs"
+
+    click_on regulator.title
+
+    expect(current_path).to eq("/songs/#{regulator.id}")
+  end
+
+  it 'shows the average song length for the artist' do
+    clutch = Artist.create!(name: "Clutch")
+    regulator = clutch.songs.create!(title: "Regulator", length: 234, play_count: 123)
+    dirt_road = clutch.songs.create!(title: "Dirt Road", length: 984, play_count: 586)
+
+    visit "/artists/#{clutch.id}/songs"
+
+    expect(page).to have_content("Average Song Length for Clutch: 609")
+  end
 end
