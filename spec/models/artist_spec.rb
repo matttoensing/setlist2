d42e71b49421c208d9e0f7595614f8f60461f258
Matt Toensing <matthew.toensing@gmail.com>
2021-07-07 14:44:28 -0700

    Add average song length method

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Artist do
   it {should have_many :songs}
+
+  describe 'instance methods' do
+    describe '#average_song_length' do
+      it 'returns the average song length' do
+        clutch = Artist.create!(name: "Clutch")
+        regulator = clutch.songs.create!(title: "Regulator", length: 234, play_count: 123)
+        dirt_road = clutch.songs.create!(title: "Dirt Road", length: 985, play_count: 586)
+        ghost = clutch.songs.create!(title: "Ghost", length: 367, play_count: 928)
+
+        expect(clutch.average_song_length.round(2)).to eq(528.67)
+      end
+    end
+  end
 end
